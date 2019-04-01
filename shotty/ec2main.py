21e18863f19ce8d29156b6fb9fbb28b1aa610d3e
Radhakrishnan Meiappan <Radhakrishnan.M@asg.com>
2019-04-01 14:07:15 +0530

    Don't start a snapshot if there is already one pending

--- shotty/ec2main.py
+++ shotty/ec2main.py
@@ -45,6 +45,10 @@ def list_snapshots(project, list_all):
                 if s.state == 'completed' and not list_all : break
     return
 
+def has_pending_snapshot(volume):
+    snapshots = list(volume.snapshots.all())
+    return snapshots and snapshots[0].state == 'pending'
+
 @cli.group('volumes')
 def volumes():
     """Commands for volumes"""
@@ -127,6 +131,9 @@ def create_snapshots(project):
         i.stop()
         i.wait_until_stopped()
         for v in i.volumes.all():
+            if has_pending_snapshot(v):
+                print("skipping {0}, snapshot already in progress".format(v.id))
+                continue
             print("    Creating snapshots of {0}".format(v.id))
             v.create_snapshot(Description="Created by snapshotalyzer 30000")
         print("Starting {0}...".format(i.id))
